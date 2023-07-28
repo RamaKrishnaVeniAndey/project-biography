29524f5a2f388e393a66f89b1709b237e319fefb
RamaKrishnaVeniAndey <137883949+RamaKrishnaVeniAndey@users.noreply.github.com>
2023-07-28 19:37:22 +0530

    Add files via upload

---
+++ design.js
@@ -0,0 +1,41 @@
+ReactDOM.render(
+    <div>
+         <center>
+        <h1> MOTHER TERESA</h1>
+        <img src="https://images.news18.com/ibnlive/uploads/2021/09/mother-teresa-16623180794x3.jpg" width="300px" height="300px"></img>
+        <p><b>Born:</b>26 August 1910, Skopje, North Macedonia</p>
+        <p><b>Died:</b>5 September 1997, Kolkata</p>
+        <p><b>parents:</b>Dranafile Bojaxhiu, Nikollë Bojaxhiu</p>
+        <p><b>Place of burial:</b>The Mother House Of The Missionaries Of Charity, Kolkata</p>
+        <p><b>Organizations founded:</b>Missionaries of Charity, MORE</p>
+        <p><b>Siblings:</b> Aga Bojaxhiu, Lazar Bojaxhiu</p>
+        <p><b>Height:</b>1.52 m</p>
+        </center>
+        <h1>Early life</h1>
+          <p>
+          Mother Teresa's given name was Anjezë Gonxhe  Bojaxhiu  Gonxhe means "rosebud" or "little flower" in Albanian. She was born on 26 August 1910 into a Kosovar Albanian family in Skopje, Ottoman Empire (now the capital of North Macedonia). She was baptised in Skopje the day after her birth. She later considered 27 August, the day she was baptised, her "true birthday".
+
+She was the youngest child of Nikollë and Dranafile Bojaxhiu (Bernai). Her father, who was involved in Albanian-community politics in Ottoman North Macedonia, died in 1919 when she was eight years old. He was born in Prizren (today in Kosovo), however, his family was from Mirdita (present-day Albania). Her mother may have been from a village near Gjakova, believed by her offspring to be Bishtazhin.
+
+According to a biography by Joan Graff Clucas, Anjezë was in her early years when she became fascinated by stories of the lives of missionaries and their service in Bengal; by age 12, she was convinced that she should commit herself to religious life. Her resolve strengthened on 15 August 1928 as she prayed at the shrine of the Black Madonna of Vitina-Letnice, where she often went on pilgrimages.
+
+            </p>  
+            <h1>Missionaries of Charity</h1>
+            <p>
+            On 10 September 1946, Teresa experienced what she later described as "the call within the call" when she travelled by train to the Loreto convent in Darjeeling from Calcutta for her annual retreat. "I was to leave the convent and help the poor while living among them. It was an order. To fail would have been to break the faith." Joseph Langford later wrote, "Though no one knew it at the time, Sister Teresa had just become Mother Teresa".
+            </p>
+            <center>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/8/85/Memorial_house_of_Mother_Teresa.jpg" width="200px" height="200px"></img>
+            </center>
+            <p>On 7 October 1950, Mother Teresa received Vatican permission for the diocesan congregation, which would become the Missionaries of Charity.[43] In her words, it would care for "the hungry, the naked, the homeless, the crippled, the blind, the lepers, all those people who feel unwanted, unloved, uncared for throughout society, people that have become a burden to the society and are shunned by everyone"</p>
+            <center>
+                <img src ="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Sisters_of_Charity.jpg/800px-Sisters_of_Charity.jpg" width="300px"    height="300"></img>
+
+                  </center>
+                  <h1>International charity</h1>
+                  <p>Mother Teresa said, "By blood, I am Albanian. By citizenship, an Indian. By faith, I am a Catholic nun. As to my calling, I belong to the world. As to my heart, I belong entirely to the Heart of Jesus." Fluent in five languages – Bengali, Albanian, Serbian, English and Hindi – she made occasional trips outside India for humanitarian reasons</p>
+                  <h1>Declining health and death</h1>
+                  <p>Mother Teresa had a heart attack in Rome in 1983 while she was visiting Pope John Paul II. Following a second attack in 1989, she received a pacemaker.[68] In 1991, after a bout of pneumonia in Mexico, she had additional heart problems. Although Mother Teresa offered to resign as head of the Missionaries of Charity, in a secret ballot the sisters of the congregation voted for her to stay, and she agreed to continue</p>
+    </div>
+    ,
+document.getElementById('content'));
